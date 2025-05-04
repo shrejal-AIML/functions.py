441a1f999aeeca05c3c6f5137ccf7f77b29a6aaf
shrejal-AIML <shrejalaries21@gmail.com>
2025-05-04 23:20:20 +0530

    Add files via upload

---
+++ arguments.py
@@ -0,0 +1,4 @@
+#positional arguments -- if you want too pass two arrgument then you have too call two function not more or less
+def my_name(fname,lname):
+    print(f"my name is {fname} {lname}")
+    my_name("shrejal","mishra")
